640e5ba316651aa0a40c13f987da7695fb50e996
brianf <brianf@081e61a2-9264-11dd-b528-c9ac4c0f0029>
2011-08-16 03:47:12 +0000

    Error handling fixes incurred during the last refactoring.

--- trunk/scmt.scm
+++ trunk/scmt.scm
@@ -9,3 +9,10 @@
 
 ; Verify too many threads won't crash wscheme
 (loop 1030 (lambda (t) (thread (sleep 1000) (displayl t #\ )))) (sleep 2000)
+
+; Call open-socket with an invalid parameter but catch the error, display message and return 'OK.
+(display
+  (call/cc (lambda (c)
+    (vector-set! ERRORS (tid) (lambda (m) (display m) (c 'OK)))
+    (open-socket 'x)
+    (display 'ERROR))))
